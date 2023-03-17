ff29eff5ec82a4b8919292d212c9b03c4981c83f
aidarbek <aidarbekabdyrakmanov@gmail.com>
2023-03-17 11:03:57 +0600

    add code

---
+++ src/main/java/ts/user/test_user/TestUserApplication.java
@@ -0,0 +1,13 @@
+package ts.user.test_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestUserApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ts/user/test_user/model/User.java
@@ -0,0 +1,18 @@
+package ts.user.test_user.model;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "firstname")
+    private String firstname;
+
+    @Column(name = "lastname")
+    private String lastname;
+
+}
