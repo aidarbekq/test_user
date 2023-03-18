7a6c71a37cfa192438bca12c7bc828a991612954
aidarbek <aidarbekabdyrakmanov@gmail.com>
2023-03-18 15:56:28 +0600

    add user service interface

---
+++ src/main/java/ts/user/test_user/service/UserService.java
@@ -0,0 +1,14 @@
+package ts.user.test_user.service;
+
+import ts.user.test_user.dto.UserRegisterDto;
+import ts.user.test_user.model.User;
+
+
+public interface UserService {
+    User register(UserRegisterDto userRegisterDto);
+
+    User findByEmail(String email);
+
+    void update(User user);
+
+}
