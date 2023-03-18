105d539d2f2563c68bb05cdf6655618f2df50452
aidarbek <aidarbekabdyrakmanov@gmail.com>
2023-03-18 15:59:06 +0600

    add response entity

---
+++ src/main/java/ts/user/test_user/exceptions/ErrorResponse.java
@@ -0,0 +1,18 @@
+package ts.user.test_user.exceptions;
+
+public class ErrorResponse {
+    private String message;
+
+    public ErrorResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
+
