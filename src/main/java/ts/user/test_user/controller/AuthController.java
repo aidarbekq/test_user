55375dd0b7ebcead40a3f09e2cc6c1b363e1ac38
aidarbek <aidarbekabdyrakmanov@gmail.com>
2023-03-18 15:57:52 +0600

    add controller

---
+++ src/main/java/ts/user/test_user/controller/AuthController.java
@@ -0,0 +1,37 @@
+package ts.user.test_user.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ts.user.test_user.dto.UserRegisterDto;
+import ts.user.test_user.exceptions.ErrorResponse;
+import ts.user.test_user.model.User;
+import ts.user.test_user.service.UserService;
+import javax.validation.Valid;
+
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody UserRegisterDto userRegisterDto) {
+        if (userService.findByEmail(userRegisterDto.getEmail()) != null) {
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body(new ErrorResponse("User with this email already exists"));
+        }
+
+        User user = userService.register(userRegisterDto);
+        return ResponseEntity.ok(user);
+    }
+
+}
+
