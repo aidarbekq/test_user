ca8a2be733d1c8678bccfbe542dca8f23659df0c
aidarbek <aidarbekabdyrakmanov@gmail.com>
2023-03-18 15:50:03 +0600

    finish user entity

--- src/main/java/ts/user/test_user/model/User.java
+++ src/main/java/ts/user/test_user/model/User.java
@@ -1,6 +1,11 @@
 package ts.user.test_user.model;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "users")
@@ -10,15 +15,61 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     @Column(name = "firstname")
     private String firstname;
 
+    @NotBlank
     @Column(name = "lastname")
     private String lastname;
 
-    @Email
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email")
+    @Column(unique = true)
     private String email;
 
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotBlank
+    @Column(name = "password")
+    private String password;
 
+    public Long getId() {
+        return id;
+    }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
 }
